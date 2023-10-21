611ac47987934e2ac5c7fc57fb3d60d7efb94c16
mmariaiv <mari.ivashova@gmail.com>
2023-10-21 20:10:54 +0300

    helpME!!!

--- src/store/useModalStore.ts
+++ src/store/useModalStore.ts
@@ -1,16 +1,21 @@
-import { create } from 'zustand'
-import { createSelectors } from './createSelectors'
+import {create} from 'zustand'
+import {createSelectors} from './createSelectors'
 
 interface ModalStore {
-    modal: string | null,
-    setModal: (modal: string | null) => void,
-    clearModal: () => void
+	modal: string | null
+	setModal: (modal: string | null) => void
+	cardModalData: any | null
+	setCardModalData: (data: any) => void
+
+	clearModal: () => void
 }
 
-  const ModalStoreBase = create<ModalStore>()((set) => ({
-    modal: null,
-    setModal: (modal) => set({ modal }),
-    clearModal: () => set({modal: null})
-  }))
+const ModalStoreBase = create<ModalStore>()((set) => ({
+	modal: null,
+	setModal: (modal) => set({modal}),
+	clearModal: () => set({modal: null}),
+	cardModalData: null,
+	setCardModalData: (data) => set({data}),
+}))
 
- export const useModalStore = createSelectors(ModalStoreBase)
+export const useModalStore = createSelectors(ModalStoreBase)
