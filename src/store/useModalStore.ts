3412fc0cb077414d9280aa39a75683a079901155
mmariaiv <mari.ivashova@gmail.com>
2023-10-22 14:58:56 +0300

    final

--- src/store/useModalStore.ts
+++ src/store/useModalStore.ts
@@ -4,8 +4,8 @@ import {createSelectors} from './createSelectors'
 interface ModalStore {
 	modal: string | undefined
 	setModal: (modal: string | undefined) => void
-	// cardModalData: any | null
-	// setCardModalData: (data: any) => void
+	cardModalData: any | null
+	setCardModalData: (data: any) => void
 	clearModal: () => void
 }
 
@@ -13,8 +13,8 @@ const ModalStoreBase = create<ModalStore>()((set) => ({
 	modal: undefined,
 	setModal: (modal) => set({modal}),
 	clearModal: () => set({modal: undefined}),
-	// cardModalData: null,
-	// setCardModalData: (data) => set(data),
+	cardModalData: null,
+	setCardModalData: (data) => set({cardModalData: data}),
 }))
 
 export const useModalStore = createSelectors(ModalStoreBase)
